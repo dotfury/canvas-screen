7475d667208b488ae57d3ac8d7bd5be88b186870
dotfury <cm2jr4@gmail.com>
2025-01-08 13:30:26 +0900

    refactor color controls and add threshold controls - 2025/01/08

--- src/components/options/pixelate.tsx
+++ src/components/options/pixelate.tsx
@@ -1,16 +1,42 @@
 import { ChangeEvent, useState } from 'react';
-import { config, type ColorTypes } from '@/effects/pixelate';
+import { config, type ColorTypes, NumberTypes } from '@/effects/pixelate';
 
 function PixelateOptions() {
   const [pixelation, setPixelation] = useState<number>(config.size);
   const [darkColor, setDarkColor] = useState<string>(config.darkColor);
   const [midColor, setMidColor] = useState<string>(config.midColor);
   const [lightColor, setLightColor] = useState<string>(config.lightColor);
+  const [darkThreshold, setDarkThreshold] = useState<number>(config.dark);
+  const [lightThreshold, setLightThreshold] = useState<number>(config.light);
+
+  const colorOptions: {
+    label: string;
+    value: string;
+    key: ColorTypes;
+  }[] = [
+    { label: 'Dark Color', value: darkColor, key: 'darkColor' },
+    { label: 'Middle Color', value: midColor, key: 'midColor' },
+    { label: 'Light Color', value: lightColor, key: 'lightColor' },
+  ];
 
   const colorsMap = {
     darkColor: setDarkColor,
     midColor: setMidColor,
     lightColor: setLightColor,
+  };
+
+  const thresholdOptions: {
+    label: string;
+    value: number;
+    key: NumberTypes;
+  }[] = [
+    { label: 'Dark Threshold', value: darkThreshold, key: 'dark' },
+    { label: 'Light Threshold', value: lightThreshold, key: 'light' },
+  ];
+
+  const thresholdMap = {
+    dark: setDarkThreshold,
+    light: setLightThreshold,
   };
 
   const handleSlider = (e: ChangeEvent<HTMLInputElement>) => {
@@ -22,6 +48,12 @@ function PixelateOptions() {
   const handleColor = (property: ColorTypes, value: string) => {
     config[property] = value;
     colorsMap[property](value);
+  };
+
+  const handleThreshold = (property: NumberTypes, value: string) => {
+    const numValue = Number(value);
+    config[property] = numValue;
+    thresholdMap[property](numValue);
   };
 
   return (
@@ -36,30 +68,28 @@ function PixelateOptions() {
           onChange={handleSlider}
         />
       </fieldset>
-      <fieldset>
-        <legend>Dark Color</legend>
-        <input
-          type="color"
-          value={darkColor}
-          onChange={(e) => handleColor('darkColor', e.currentTarget.value)}
-        />
-      </fieldset>
-      <fieldset>
-        <legend>Middle Color</legend>
-        <input
-          type="color"
-          value={midColor}
-          onChange={(e) => handleColor('midColor', e.currentTarget.value)}
-        />
-      </fieldset>
-      <fieldset>
-        <legend>Light Color</legend>
-        <input
-          type="color"
-          value={lightColor}
-          onChange={(e) => handleColor('lightColor', e.currentTarget.value)}
-        />
-      </fieldset>
+      {colorOptions.map(({ label, value, key }) => (
+        <fieldset>
+          <legend>{label}</legend>
+          <input
+            type="color"
+            value={value}
+            onChange={(e) => handleColor(key, e.currentTarget.value)}
+          />
+        </fieldset>
+      ))}
+      {thresholdOptions.map(({ label, value, key }) => (
+        <fieldset>
+          <legend>{label}</legend>
+          <input
+            type="range"
+            value={value}
+            min="0"
+            max="255"
+            onChange={(e) => handleThreshold(key, e.currentTarget.value)}
+          />
+        </fieldset>
+      ))}
     </>
   );
 }
