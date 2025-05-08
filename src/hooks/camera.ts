6ee9bd52b50d279ede2d64a234cdd7a2886834be
dotfury <cm2jr4@gmail.com>
2025-05-08 15:06:45 +0900

    testing error - 2025/05/08

--- src/hooks/camera.ts
+++ src/hooks/camera.ts
@@ -16,6 +16,7 @@ export function useCamera(): [Camera | null, boolean] {
 
         setCamera(camera);
       } catch (error: any) {
+        console.log('error: ', error);
         setCameraError(true);
       }
     };
