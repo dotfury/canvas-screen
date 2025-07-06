6d0e972f114cc13e4d2c7a21887faa236f0b2aef
dotfury <cm2jr4@gmail.com>
2025-07-06 14:39:50 +0900

    Pwa (#20)
    * setup pwa configuration - 2025/07/06
    
    * add pwa assets - 2025/07/06

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,63 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'maskable-icon-512x512.svg',
+      ],
+      manifest: {
+        name: 'Canvas Screen',
+        short_name: 'Canvas Screen',
+        description: 'Canvas filters for camera',
+        theme_color: '#242424',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshots/desk1.jpg',
+            sizes: '1280x720',
+            type: 'image/jpg',
+            form_factor: 'wide',
+            label: 'Example of pixelate filter',
+          },
+          {
+            src: 'screenshots/desk2.jpg',
+            sizes: '1280x720',
+            type: 'image/jpg',
+            form_factor: 'wide',
+            label: 'Example of ascii filter',
+          },
+          {
+            src: 'screenshots/mobile.jpg',
+            sizes: '327x706',
+            type: 'image/jpg',
+            form_factor: 'narrow',
+            label: 'Example of mobile screen',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+      },
+    }),
+  ],
 });
