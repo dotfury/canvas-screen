85a6841ac0d220b08aa3a4e9b4ef0459f75c09d1
dotfury <cm2jr4@gmail.com>
2025-12-03 20:37:33 +0900

    use dropdown for effect options - 2025/12/03

--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState, useContext } from 'react';
+import { useState, useContext, ChangeEvent } from 'react';
 import { effectsList, EFFECTS } from '@/utils/effectList';
 import { AppContext } from '@/context/appContext';
 import Options from '@/components/options';
@@ -10,8 +10,8 @@ function Controls() {
     camera?.effect || EFFECTS.STANDARD
   );
 
-  const handleEffect = (e: MouseEvent<HTMLButtonElement>) => {
-    const effect = e.currentTarget.dataset.id!;
+  const handleEffect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const effect = e.currentTarget.value;
 
     setEffect(effect);
     camera?.setEffect(effect);
@@ -21,21 +21,20 @@ function Controls() {
     <>
       <div className="md:flex md:flex-1 md:gap-2">
         <div className="flex pl-2 gap-2 flex-1 lg:block">
-          {effectsList.map((effectName) => {
-            const activeClassName = effect === effectName ? 'active' : '';
-
-            return (
-              <button
-                className={`standard-button block ${activeClassName}`}
-                key={effectName}
-                onClick={handleEffect}
-                data-id={effectName}
-                disabled={appContext?.showOverlay}
-              >
-                {effectName}
-              </button>
-            );
-          })}
+          <fieldset>
+            <label htmlFor="effect">Effect: </label>
+            <select
+              name="effect"
+              id="effect-selector"
+              value={effect}
+              onChange={handleEffect}
+              disabled={appContext?.showOverlay}
+            >
+              {effectsList.map((effectName) => {
+                return <option value={effectName}>{effectName}</option>;
+              })}
+            </select>
+          </fieldset>
         </div>
       </div>
       <Options currentEffect={effect} />
